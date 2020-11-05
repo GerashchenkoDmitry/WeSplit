d52348f2362d5686b73d215043d30fba979d8e13
Dmitry Gerashchenko <finiqw@gmail.com>
2020-11-05 13:12:47 +0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Дмитрий Геращенко on 05.11.2020.
+//  Copyright © 2020 Дмитрий Геращенко. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
